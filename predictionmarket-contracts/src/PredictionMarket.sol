a32f8e8aae3fd5606e27cb4721f9afe49663d1df
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2025-03-23 14:13:46 +0800

    feat: allow fee withdrawals from market

--- predictionmarket-contracts/src/PredictionMarket.sol
+++ predictionmarket-contracts/src/PredictionMarket.sol
@@ -27,6 +27,7 @@ contract PredictionMarket is Ownable, ReentrancyGuard {
     );
     event MarketResolved(uint256 indexed marketId, bool result);
     event FeesUpdated(uint256 creationFee, uint256 tradingFee);
+    event MarketFeesWithdrawn(uint256 indexed marketId, uint256 amount);
 
     constructor() Ownable(msg.sender) {
         creationFee = 100; // 1%
@@ -89,6 +90,49 @@ contract PredictionMarket is Ownable, ReentrancyGuard {
         emit FeesUpdated(_creationFee, _tradingFee);
     }
 
+    // Withdraw fees from a specific market
+    function withdrawMarketFees(uint256 marketId) external onlyOwner {
+        address marketAddress = markets[marketId];
+        require(marketAddress != address(0), "Market does not exist");
+
+        Market market = Market(payable(marketAddress));
+        uint256 feesBeforeWithdraw = market.accumulatedFees();
+        require(feesBeforeWithdraw > 0, "No fees to withdraw");
+
+        // First withdraw from market to this contract
+        market.withdrawFees();
+
+        // Then transfer to owner
+        payable(owner()).transfer(feesBeforeWithdraw);
+
+        emit MarketFeesWithdrawn(marketId, feesBeforeWithdraw);
+    }
+
+    // Withdraw fees from multiple markets
+    function withdrawMarketFeesBatch(
+        uint256[] calldata marketIds
+    ) external onlyOwner {
+        uint256 totalWithdrawn = 0;
+
+        for (uint256 i = 0; i < marketIds.length; i++) {
+            address marketAddress = markets[marketIds[i]];
+            if (marketAddress != address(0)) {
+                Market market = Market(payable(marketAddress));
+                uint256 marketFees = market.accumulatedFees();
+                if (marketFees > 0) {
+                    market.withdrawFees();
+                    totalWithdrawn += marketFees;
+                    emit MarketFeesWithdrawn(marketIds[i], marketFees);
+                }
+            }
+        }
+
+        if (totalWithdrawn > 0) {
+            payable(owner()).transfer(totalWithdrawn);
+        }
+    }
+
+    // Withdraw all accumulated fees from PredictionMarket contract
     function withdrawFees() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
